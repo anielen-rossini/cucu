41fd790cbdd21d712b3fef6d55bf9c705755e39a
Aluno <anielenrossini12@gmail.com>
2020-03-02 09:36:53 -0300

    aula html

---
+++ aua3.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <style media= "screen">
+  .noticias {
+
+  }
+  td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+  </style>
+</head>
+<body>
+  <h2> Portal de Notícias IFPR </h2>
+  <div class="noticias">
+    <h2> Pai de Saulo Pôncio confirma fim do Casamento de filho com Gabi Brandt </h2>
+    <p> Durante todo o domingo (01/03), a web especulou sobre a separação definitiva entre Saulo Pôncio e Gabi Brandt. O nome dos dois esteve nos Trending Topics do Twitter durante a tarde, e no começo da noite, houve a confirmação: eles já são um ex-casal.</p>
+    <p> Quem deu a notícia foi o pai de Saulo, o pastor Márcio Pôncio, em declaração enviada ao blog Gossip do Dia. Ele foi questionado a respeito da situação e deu uma longa resposta. </p>
+    <p> “Gabi e Saulo são jovens, são pessoas como vocês dizem, subcelebridades, casaram-se muito cedo afim de esquecerem seu passado, ao invés de construir um futuro familiar. Mas agora já foi”, começou o pastor Márcio. “Assim como casaram muito rápido também estão vivendo as crises de um relacionamento a dois também muito rápido”, ponderou. </p>
+    <img src= "https://cenapop.uol.com.br/wordpress/wp-content/uploads/2019/07/saulo_rep_inst_00.jpg">
+    <h6>
+      POR REDAÇÃO
+      1 DE MARÇO DE 2020
+      <h6>
+      </div>
+
+
+      <div class="noticias">
+        <h2> Os brasileiros que mais faturam no Instagram </h2>
+        <p> O Instagram virou uma fonte de renda para muitos influenciadores digitais, inclusive brasileiros. Levantamento da Hopper HQ mostra a lista de famosos que mais faturam na rede social de Mark Zueckerberg e tem artista do Brasil na relação, com nomes que Neymar a Gracyanne Barbosa. </p>
+        <p> Os números são exorbitantes, mesmo no ambiente de celebridades e surpreendem. O cálculo foi feito por uma das grandes agências de Marketing Digital dos EUA, a Hopper HQ, e apresentou os resultados em dólares. Confira os brasileiros que aparecem no ranking. </p>
+
+
+        <table>
+          <tr>
+            <th>Nome</th>
+            <th>Seguidores</th>
+            <th>Ganho</th>
+          </tr>
+          <tr>
+            <td>Neymar</td>
+            <td>132 milhões</td>
+            <td>R$ 3,2 milhões</td>
+          </tr>
+          <tr>
+            <td>Ronaldinho Gaúcho</td>
+            <td>51,4 milhões</td>
+            <td>R$1,15 milhão</td>
+          </tr>
+          <tr>
+            <td>Camila Coelho</td>
+            <td>8,6 milhões</td>
+            <td>R$125 mil</td>
+          </tr>
+          <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+          </tr>
+          <h6>
+            Publicado em 01/03/2020 às 09:00:20 Por: Redação NT
+            <h6>
+        </table>
+      </div>
+      <div class = "noticias">
+   <h2> Guilherme, Pyong e Gizelly estão no sexto paredão do BBB 20 </h2>
+    <p> Gizelly, Guilherme e Pyong estão no sexto paredão do BBB 20, formado na noite de hoje. Prior venceu a prova bate e volta e escapou da berlinda. Anjo da semana, Guilherme imunizou Flayslane. Em seguida, o modelo foi indicado por Ivy, e escolheu Pyong para ir ao paredão junto com ele. Prior foi o mais votado pela casa, com seis votos, e escolheu Gizelly para encarar a berlinda. </p>
+    <p> Gizelly, Prior e Pyong puderam disputar a prova bate e volta. A dinâmica era de sorte. "Serão quatro rodadas, vocês deverão pegar uma carta e virar, quem pegar a carta mais alta, marca um ponto", explicou o apresentador. </p>
+    <img src= "https://conteudo.imguol.com.br/c/entretenimento/ad/2020/03/02/montagem---paredao-1583118270823_v2_900x506.jpg">
+      </div>
+    </body>
+    </html>
