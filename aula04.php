39a5e044a9ed19d302d2fde1d3bc076fb209135a
anielen rossini <anielenrossini12@gmail.com>
2020-03-09 09:22:34 -0300

    aula 04

---
+++ aula04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Aula 04 - CSS e HTML</title>
+     <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+     <!-- Toda importação de css deve ser feita no head -->
+    <link rel="stylesheet" href="aula04.css">
+  </head>
+  <body>
+<div class= "site">
+   <div class= "topo">
+     <h3> Romace</h3>
+   </div>
+       <div class= "conteudo">
+         Corpo do site
+      </div>
+      <div class= "rodape">
+        Desenvolvido por Anielen Rosssini
+      </div>
+
+</div>
+  </body>
+</html>
